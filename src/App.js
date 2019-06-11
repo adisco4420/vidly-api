77094dcf2fc6a0170757dec866d5373ee377090e
username <adisco4420@gmail.com>
2019-06-11 19:01:34 +0100

    switch and route

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
+import Products from './components/routing/products';
 // import Counter from './components/counter-app/counter';
 // import HomeVid from './components/vivdy/HomeVid';
 import Navbar from './components/routing/navbar';
+import Home from './components/routing/home';
+import Dashboard from './components/routing/admin/dashboard';
+import Posts from './components/routing/posts'
 
 function App() {
   return (
     // <Counter />
     // <HomeVid />
+    <div>
     <Navbar />
+    <div className="content">
+      <Switch>
+        <Route path="/products" component={Products} />
+        <Route path="/posts" component={Posts} />
+        <Route path="/admin" component={Dashboard} />
+        <Route path="/" component={Home} />
+      </Switch>
+    
+    </div>
+    </div>
   );
 }
 
