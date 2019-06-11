b2ca1e92bd184c2eacff50226cbbce919f1a4bae
username <adisco4420@gmail.com>
2019-06-11 18:44:11 +0100

    use navbar in app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
-import Counter from './components/counter-app/counter';
-import HomeVid from './components/vivdy/HomeVid';
+// import Counter from './components/counter-app/counter';
+// import HomeVid from './components/vivdy/HomeVid';
+import Navbar from './components/routing/navbar';
 
 function App() {
   return (
     // <Counter />
-    <HomeVid />
+    // <HomeVid />
+    <Navbar />
   );
 }
 
